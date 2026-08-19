9c45fc4ad7e411d8f85e13911f7939de2326c0ec
Sanskar <sanskarin@outlook.in>
2026-08-19 08:45:47 +0530

    build: wire Android splash screen library

--- androidApp/build.gradle.kts
+++ androidApp/build.gradle.kts
@@ -52,6 +52,7 @@ android {
 dependencies {
     implementation(project(":shared"))
     implementation(libs.androidx.core.ktx)
+    implementation(libs.androidx.core.splashscreen)
     implementation(libs.androidx.activity.compose)
     implementation(libs.androidx.lifecycle.runtime.ktx)
     implementation(libs.androidx.lifecycle.viewmodel.ktx)
