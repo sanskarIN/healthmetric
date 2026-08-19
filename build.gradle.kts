f31899084dbcb3dbd73359be50eae6936ab107cb
Sanskar <sanskarin@outlook.in>
2026-08-19 08:34:55 +0530

    fix: remove undefined plugin and enable Kotlin lint for modules

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,5 +4,9 @@ plugins {
     alias(libs.plugins.kotlin.android) apply false
     alias(libs.plugins.kotlin.multiplatform) apply false
     alias(libs.plugins.kotlin.compose) apply false
-    alias(libs.plugins.kotlin.serialization) apply false
+    alias(libs.plugins.ktlint) apply false
+}
+
+subprojects {
+    apply(plugin = "org.jlleitschuh.gradle.ktlint")
 }
