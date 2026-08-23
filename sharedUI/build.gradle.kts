f5ebac7522cba1333e54230fe04d6b17c3771faa
Sanskar <sanskarin@outlook.in>
2026-08-23 13:38:44 +0530

    test(shared-ui): enable common unit tests

--- sharedUI/build.gradle.kts
+++ sharedUI/build.gradle.kts
@@ -39,5 +39,8 @@ kotlin {
             implementation(compose.material3)
             implementation(compose.ui)
         }
+        commonTest.dependencies {
+            implementation(kotlin("test"))
+        }
     }
 }
